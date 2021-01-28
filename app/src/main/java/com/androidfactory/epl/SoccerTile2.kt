2a6ead8cbb7dc393350235b8b16d46e5d04b8d1a
developer-dmp <developer.dmp@gmail.com>
2021-01-28 12:40:45 -0500

    Added image url to SoccerTile2.kt

--- app/src/main/java/com/androidfactory/epl/SoccerTile2.kt
+++ app/src/main/java/com/androidfactory/epl/SoccerTile2.kt
@@ -3,5 +3,6 @@ package com.androidfactory.epl
 data class SoccerTile2(
     val id: String = "",
     val title: String = "",
-    val description: String = ""
+    val description: String = "",
+    val imageUrl: String = ""
 )
